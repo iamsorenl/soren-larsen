5c30fb9ad3904d084f04ff6f5805c110747ba013
Soren Larsen <iamsorenl@gmail.com>
2024-09-25 23:32:08 -0700

    updated personal portfolio site with info as of sept 25 2024

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -6,13 +6,16 @@ import Stack from '@mui/material/Stack';
 import CardMedia from '@mui/material/CardMedia';
 import Box from '@mui/material/Box';
 import about from '../data/about';
-import headshot1 from '../images/Headshot1.jpeg';
-import headshot2 from '../images/Headshot2.jpeg';
-import wave from '../images/Wave.jpeg';
+import headshot1 from '../images/Headshot1.jpg';
+import headshot2 from '../images/Headshot2.jpg';
+import headshot3 from '../images/Headshot3.jpg';
+import headshot4 from '../images/Headshot4.jpg';
+import headshot5 from '../images/Headshot5.jpg';
 import turtle from '../images/Turtle.jpg';
-import surf from '../images/Surf.jpeg';
+import surf from '../images/Surf.jpg';
+import halfdome from '../images/HalfDome.jpg';
 
-const imageUrls = [headshot1, headshot2, wave, turtle, surf];
+const imageUrls = [headshot5, headshot1, halfdome, headshot2, turtle, headshot3, headshot4, surf];
 
 const AboutCard = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
