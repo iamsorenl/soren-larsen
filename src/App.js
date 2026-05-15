1ee8fd56c72e65fabb5b27d569390b4485312295
Soren Larsen <iamsorenl@gmail.com>
2026-05-14 18:50:07 -0700

    feat(app): mount ChatWidget at root

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Body from './components/Body';
 import ErrorBoundary from './components/ErrorBoundary';
+import ChatWidget from './components/chat/ChatWidget';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { Box } from '@mui/material';
 
@@ -15,6 +16,7 @@ function App() {
           <Box sx={{ pt: 8 }}>
             <Body />
           </Box>
+          <ChatWidget />
         </div>
       </ErrorBoundary>
     </ThemeProvider>
